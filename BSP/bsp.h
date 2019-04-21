0b23d4a0e40770877dbb91b68186b3ad3dce2a13
supgb <supgb96@gmail.com>
2019-04-22 02:19:32 +0800

    Blueprint for multiple board support feature

---
+++ BSP/bsp.h
@@ -0,0 +1,13 @@
+/* 
+Author: supgb
+Date: Apr. 22, 2019 
+Usage: It contains definitions about what embedded controller we
+    are working with. 
+ */
+
+#ifndef __BSP_H
+#define __BSP_H
+
+#include "stdint.h"
+
+#endif // __BSP_H
